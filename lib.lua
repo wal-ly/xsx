5277eb9954cc01d4ed40cffc2c25d4beb39b1a14
walle <100832774+wal-ly@users.noreply.github.com>
2023-08-10 18:10:03 +0500

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -73,7 +73,7 @@ local drag = function(obj, latency)
 end
 
 local library = {
-    version = "2.0.2",
+    version = "1.0.0",
     title = title or "xsx " .. tostring(math.random(1,366)),
     fps = 0,
     rank = "private"
